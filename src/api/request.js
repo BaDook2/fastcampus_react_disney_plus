d1571f2dabb263ccbc41d7f6a0849248f15e42c3
BaDook2 <wkdrudqls114@gmail.com>
2024-04-04 18:52:48 +0900

    home

--- src/api/request.js
+++ src/api/request.js
@@ -1,5 +1,5 @@
 const requests = {
-  fetchNowPlaying: "movie/now_playing",
+  fetchNowPlaying: "/movie/now_playing",
   fetchTranding:"/trending/all/week",
   fetchTopRated:"/movie/top_rated",
   fetchActionMovies:"/discover/movie?with_genres=28",
